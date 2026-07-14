0880717ce792fbae45855be7f218cab54652c94c
jalla <jallasimhadri@gmail.com>
2026-07-14 11:49:58 +0530

    update

--- script.js
+++ script.js
@@ -57,25 +57,25 @@ AOS.init({
 
 
 
-const form = document.querySelector(".contact-form");
+// const form = document.querySelector(".contact-form");
 
-form.addEventListener("submit", async function(e) {
-  e.preventDefault();
+// form.addEventListener("submit", async function(e) {
+//   e.preventDefault();
 
-  const formData = new FormData(form);
+//   const formData = new FormData(form);
 
-  const response = await fetch(form.action, {
-    method: "POST",
-    body: formData
-  });
+//   const response = await fetch(form.action, {
+//     method: "POST",
+//     body: formData
+//   });
 
-  if (response.ok) {
-    alert("✅ Message Sent Successfully!");
-    form.reset();
-  } else {
-    alert("❌ Something went wrong!");
-  }
-});
+//   if (response.ok) {
+//     alert("✅ Message Sent Successfully!");
+//     form.reset();
+//   } else {
+//     alert("❌ Something went wrong!");
+//   }
+// });
 
 
 
